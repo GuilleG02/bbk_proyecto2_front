e8f66aed7455ad78605da3fed8b2ac229b03a047
GuilleG02 <guille.g0220rgmail.com>
2025-09-06 12:47:41 +0200

    Proteccion de Rutas

--- redux-intro/src/App.jsx
+++ redux-intro/src/App.jsx
@@ -1,13 +1,20 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 import AuthForms from './components/AuthForms'
+import Home from './components/Home'
 import Profile from './components/Profile'
 import EditProfile from './components/EditProfile'
-import Home from './components/Home'
 import PostDetail from './components/PostDetail'
 import SearchPage from './components/SearchPage'
+import PostForm from './components/PostForm'
 import TheHeader from './components/TheHeader'
-import PrivateRoute from './components/PrivateRoute'
-import PostForm from './components/PostForm' // importamos PostForm
+
+function PrivateRoute({ children }) {
+  const { user } = useSelector((state) => state.auth)
+  if (!user) return <Navigate to="/auth" replace />
+  return children
+}
 
 function AppContent() {
   const location = useLocation()
@@ -20,18 +27,15 @@ function AppContent() {
       <main style={{ paddingTop: showHeader ? '100px' : '0' }}>
         <Routes>
           <Route path="/auth" element={<AuthForms />} />
-
           <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
           <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
           <Route path="/edit-profile" element={<PrivateRoute><EditProfile /></PrivateRoute>} />
           <Route path="/posts/:id" element={<PrivateRoute><PostDetail /></PrivateRoute>} />
-
-          {/* Rutas para crear y editar posts */}
           <Route path="/create-post" element={<PrivateRoute><PostForm /></PrivateRoute>} />
           <Route path="/edit-post/:id" element={<PrivateRoute><PostForm /></PrivateRoute>} />
-
           <Route path="/search" element={<PrivateRoute><SearchPage /></PrivateRoute>} />
           <Route path="/search/:searchType/:searchText?" element={<PrivateRoute><SearchPage /></PrivateRoute>} />
+          <Route path="*" element={<PrivateRoute><Navigate to="/" replace /></PrivateRoute>} />
         </Routes>
       </main>
     </>
